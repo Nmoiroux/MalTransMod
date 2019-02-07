8e0982ced7b0d14db3e95255681ceb30e9458c65
Nicolas MOIROUX <Nicolas.Moiroux@ird.fr>
2019-02-07 17:24:53 +0000

    some change to work with VLAIB function. But failed at LHS function...

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -3,55 +3,54 @@ library(pse)
 
 
 
-### modify fRTP function to be fed with parameters of VC_bincoef function
-fRTP_sens <- function(nsim,S = 0.9, g = 3, Pop = 1000, prev = 0.5, k = 0.1, n = 11, 
-                                                                                    m_pre_h_u,
-                                                                                    #m_pre_h_p, 
-                                                                                    m_post_h_u,
-                                                                                    m_post_h_p,
-                                                                                    f_h_u_live,
-                                                                                    #f_h_rr_live,
-                                                                                    #Ch,
-                                                                                    #pii, 
-                                                                                    #pref_h_u,  
-                                                                                    FUN){
-  RTP<- FUN(nsim,S = S, g = g, Pop = Pop, prev = prev, k = k, n = n, 
-            m_pre_h_u=m_pre_h_u,
-            #m_pre_h_p=m_pre_h_p, 
-            m_post_h_u= m_post_h_u,
-            m_post_h_p=m_post_h_p,
-            f_h_u_live=f_h_u_live,
-            #f_h_rr_live=f_h_rr_live,
-            #Ch=Ch,
-            #pii=pii, 
-            pref_h_u=0.3
-            )["VLAIB",] / +
-        FUN(nsim,S = S, g = g, Pop = Pop, prev = prev, k = k, n = n, 
-            m_pre_h_u=m_pre_h_u,
-            #m_pre_h_p=m_pre_h_p, 
-            m_post_h_u= m_post_h_u,
-            m_post_h_p=m_post_h_p,
-            f_h_u_live=f_h_u_live,
-            #f_h_rr_live=f_h_rr_live,
-            #Ch=Ch,
-            #pii=pii, 
-            pref_h_u=0.5
-            )["VLAIB",]
-  return(RTP)
+### modify fRTP function to be fed with all parameters of VLAIB function
+fRTP_sens <- function(nsim,S = 0.9, g = 3, Nh = 1000, Ih = 0.5, k = 0.1, n = 11,  m1u = 0.016,
+                                                                                    #m1p, 
+                                                                                    m2u = 0.005,
+                                                                                    m2p = 0.21,
+																																										fi1_u = 0.55,
+                                                                                    #RR_fi1,
+                                                                                    #Uh,
+                                                                                    #pi, 
+                                                                                    #Pllin,  
+                                                                                    FUN = VLAIB){
+  RTP<- FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
+            m1u=m1u,
+            #m1p=m1p, 
+            m2u=m2u,
+            m2p=m2p,
+  					fi1_u=fi1_u,
+            #RR_fi1=RR_fi1,
+            #Uh=Uh,
+            #pi=pi, 
+            Pllin=0.7
+            )["VLAIB"] / +
+        FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
+        		m1u=m1u,
+        		#m1p=m1p, 
+        		m2u=m2u,
+        		m2p=m2p,
+        		fi1_u=fi1_u,
+        		#RR_fi1=RR_fi1,
+        		#Uh=Uh,
+        		#pi=pi, 
+        		Pllin=0.5
+            )["VLAIB"]
+  return(-(1-RTP)*100)
 }
 
 ##### following tutorial of pse package
 ## names of the parameters
-factors <- c("S", "g", "Pop", "prev", "k", "n", 
-             "m_pre_h_u" , # 0 - 0.04 (EHT Moiroux)
-             #"m_pre_h_p",  # 0.1 - 1 (EHT Moiroux)           # TESTED
-             "m_post_h_u", # 0 - 0.01 (EHT Moiroux)
-             "m_post_h_p", # 0 - 0.5 (EHT Moiroux)
-             "f_h_u_live" # 0.2 - 0.8 (EHT Moiroux)
-             #"f_h_rr_live", # 0.25 - 0.9 (EHT Moiroux)       # TESTED
-             #"Ch", # 0 - 1                                  # TESTED
-             #"pii", # 0 - 1                                 # TESTED
-             #"pref_h_u" # 0.3 - 0.9                        # TESTED)
+factors <- c("S", "g", "Nh", "Ih", "k", "n", 
+             "m1u" , # 0 - 0.04 (EHT Moiroux)
+             #"m1p",  # 0.1 - 1 (EHT Moiroux)           # TESTED
+             "m2u", # 0 - 0.01 (EHT Moiroux)
+             "m2p", # 0 - 0.5 (EHT Moiroux)
+             "fi1_u" # 0.2 - 0.8 (EHT Moiroux)
+             #"RR_fi1", # 0.25 - 0.9 (EHT Moiroux)       # TESTED
+             #"Uh", # 0 - 1                                  # TESTED
+             #"pi", # 0 - 1                                 # TESTED
+             #"Pllin" # 0.3 - 0.9                        # TESTED)
              ) 
 
 
@@ -93,50 +92,44 @@ q.arg <- list(list(min=0.7, max=0.95), #S
               #list(min=0.2, max=0.4)   #pref_h_u
               )
 
-# function that calculate de reduction in RTP
-oneRun <- function(S, g, Pop, prev, k, n,
-                   m_pre_h_u,
-                   #m_pre_h_p, 
-                   m_post_h_u,
-                   m_post_h_p,
-                   f_h_u_live#,
-                   #f_h_rr_live,
-                   #Ch,
-                   #pii, 
-                   #pref_h_u
-                   ){
-  x <- -1/ fRTP_sens(nsim,S, g, Pop, prev, k, n, 
-                     m_pre_h_u,
-                     #m_pre_h_p, 
-                     m_post_h_u,
-                     m_post_h_p,
-                     f_h_u_live#,
-                     #f_h_rr_live,
-                     #Ch,
-                     #pii, 
-                     #pref_h_u
-                     , FUN)
-  return(x)
-  }
 
-# applicate oneRun on a dataframe
+# function modelRun encapsulates fRTP_sens function,in a manner to receive a data.frame containing 
+# all parameter combinations and returning the results in one array.
 modelRun <- function (my.data) {
-  return(mapply(oneRun, my.data[,1], my.data[,2], my.data[,3], my.data[,4], my.data[,5], my.data[,6],
+  return(mapply(fRTP_sens, my.data[,1], my.data[,2], my.data[,3], my.data[,4], my.data[,5], my.data[,6],
                 my.data[,7], my.data[,8], my.data[,9], my.data[,10]
                 #, my.data[,11], my.data[,12], my.data[,13], my.data[,14], my.data[,15]
                 ))
-  }
+}
+
+# Generates the Latin Hypercube sampling for uncertainty and sensitivity analyses.
 myLHS <- LHS(modelRun, factors, 500, q, q.arg, nboot=50)
+
+# accessing the data and result data frames from an LHS object
 get.data(myLHS)
+
+# plots the empirical cumulative density function
 plotecdf(myLHS)
+
+# produces a series of scatterplots from data
 plotscatter(myLHS)
+
+# plots the partial rank correlation coefficient from an LHS object
 plotprcc(myLHS)
+
+# Estimates the partial inclination coefficient of a model response in relation with all model input variables
 pic(myLHS, nboot=40)
+
+# In order to decide whether our sample size was adequate or insufficient, we calculate
+# the Symmetric Blest Measure of Agreement (SBMA) between the PRCC coefficients
+# of two runs with different sample sizes.
 newLHS <- LHS(modelRun, factors, 750, q, q.arg)
 (mySbma <- sbma(myLHS, newLHS))
 
-### define possible range of parameters
 
+
+
+### define possible range of parameters from field data
 boxplot(subset(Data_EHT, ITN=="no")$m_pre_h_u.p)
 boxplot(subset(Data_EHT, ITN=="LLIN")$m_pre_h_u.p)
 boxplot(subset(Data_EHT, ITN=="no")$m_post_h_u.p)
