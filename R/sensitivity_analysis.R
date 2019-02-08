840629f5eac1eb6810bd546107f791aa96712ae6
Nicolas MOIROUX <Nicolas.Moiroux@ird.fr>
2019-02-08 09:36:49 +0000

    WORKS ! Continue to rewrite parameter name in comment. Add value for parameter nsim (100) in function modelRun before my.data[,1]

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -5,98 +5,100 @@ library(pse)
 
 ### modify fRTP function to be fed with all parameters of VLAIB function
 fRTP_sens <- function(nsim,S = 0.9, g = 3, Nh = 1000, Ih = 0.5, k = 0.1, n = 11,  m1u = 0.016,
-                                                                                    #m1p, 
-                                                                                    m2u = 0.005,
-                                                                                    m2p = 0.21,
-																																										fi1_u = 0.55,
-                                                                                    #RR_fi1,
-                                                                                    #Uh,
-                                                                                    #pi, 
-                                                                                    #Pllin,  
-                                                                                    FUN = VLAIB){
-  RTP<- FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
-            m1u=m1u,
-            #m1p=m1p, 
-            m2u=m2u,
-            m2p=m2p,
-  					fi1_u=fi1_u,
-            #RR_fi1=RR_fi1,
-            #Uh=Uh,
-            #pi=pi, 
-            Pllin=0.7
-            )["VLAIB"] / +
-        FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
-        		m1u=m1u,
-        		#m1p=m1p, 
-        		m2u=m2u,
-        		m2p=m2p,
-        		fi1_u=fi1_u,
-        		#RR_fi1=RR_fi1,
-        		#Uh=Uh,
-        		#pi=pi, 
-        		Pllin=0.5
-            )["VLAIB"]
+                                                                                  #m1p, 
+                                                                                  m2u = 0.005,
+                                                                                  m2p = 0.21,
+																																									fi1_u = 0.55,
+                                                                                  #RR_fi1,
+                                                                                  #Uh,
+                                                                                  #pi, 
+                                                                                  #Pllin,  
+                                                                                  FUN = VLAIB){
+													  RTP <- FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
+																																			            m1u=m1u,
+																																			            #m1p=m1p, 
+																																			            m2u=m2u,
+																																			            m2p=m2p,
+																																			  					fi1_u=fi1_u,
+																																			            #RR_fi1=RR_fi1,
+																																			            #Uh=Uh,
+																																			            #pi=pi, 
+																																			            Pllin=0.7
+													            )["VLAIB"] / +
+													        FUN(nsim,S = S, g = g, Nh = Nh, Ih = Ih, k = k, n = n, 
+													        		m1u=m1u,
+													        		#m1p=m1p, 
+													        		m2u=m2u,
+													        		m2p=m2p,
+													        		fi1_u=fi1_u,
+													        		#RR_fi1=RR_fi1,
+													        		#Uh=Uh,
+													        		#pi=pi, 
+													        		Pllin=0.5
+													            )["VLAIB"]
   return(-(1-RTP)*100)
 }
 
 ##### following tutorial of pse package
 ## names of the parameters
 factors <- c("S", "g", "Nh", "Ih", "k", "n", 
-             "m1u" , # 0 - 0.04 (EHT Moiroux)
-             #"m1p",  # 0.1 - 1 (EHT Moiroux)           # TESTED
-             "m2u", # 0 - 0.01 (EHT Moiroux)
-             "m2p", # 0 - 0.5 (EHT Moiroux)
-             "fi1_u" # 0.2 - 0.8 (EHT Moiroux)
+             "m1u" ,    # 0 - 0.04 (EHT Moiroux)
+             #"m1p",    # 0.1 - 1 (EHT Moiroux)          # TESTED
+             "m2u",     # 0 - 0.01 (EHT Moiroux)
+             "m2p",     # 0 - 0.5 (EHT Moiroux)
+             "fi1_u"    # 0.2 - 0.8 (EHT Moiroux)
              #"RR_fi1", # 0.25 - 0.9 (EHT Moiroux)       # TESTED
-             #"Uh", # 0 - 1                                  # TESTED
-             #"pi", # 0 - 1                                 # TESTED
-             #"Pllin" # 0.3 - 0.9                        # TESTED)
+             #"Uh",     # 0 - 1                          # TESTED
+             #"pi",     # 0 - 1                          # TESTED
+             #"Pllin"   # 0.1 - 0.7                      # TESTED)
              ) 
 
 
-## the probability density functions
+## the probability density functions for each parameter
 # discrete uniform probablity function (for parameters g and n)
 qdunif<-function(p, min, max) floor(qunif(p, min, max))
 
-q <- c("qunif", #S
-       "qdunif",#g
-       "qunif", #pop
-       "qunif", #prev
-       "qunif", #k
+q <- c("qunif",  #S
+       "qdunif", #g
+       "qunif",  #Nh
+       "qunif",  #Ih
+       "qunif",  #k
        "qdunif", #n
-       "qunif", #m_pre_h_u
-       #"qunif", #m_pre_h_p # TESTED (fixed)
-       "qunif", # m_post_h_u
-       "qunif", # m_post_h_p
-       "qunif"#, #f_h_u_live
-       #"qunif", # f_h_rr_live # TESTED (fixed)
-       #"qunif", # Ch # TESTED (fixed)
-       #"qunif", # pii # TESTED (fixed)
-       #"qunif"  #pref_h_u
+       "qunif",  #m1u
+       #"qunif", #m1p # TESTED (fixed)
+       "qunif",  #m2u
+       "qunif",  #m2p
+       "qunif"#, #fi1_u
+       #"qunif", #RR_fi1 # TESTED (fixed)
+       #"qunif", #Uh # TESTED (fixed)
+       #"qunif", #pi # TESTED (fixed)
+       #"qunif"  #Pllin
        )
+
+
 ## a list containing the lists with all the parameters to the density functions
 q.arg <- list(list(min=0.7, max=0.95), #S
               list(min=2, max=6),      #g
-              list(min=300, max=5000), #pop
-              list(min=0.1, max=0.8),  #prev
+              list(min=300, max=5000), #Nh
+              list(min=0.1, max=0.8),  #Ih
               list(min=0.02, max=0.2), #k
               list(min=8, max=16),     #n
-              list(min=0, max=0.04),   #m_pre_h_u
-              #list(min=0.1, max=0.9), #m_pre_h_p # TESTED (fixed)
-              list(min=0, max=0.02),  # m_post_h_u
-              list(min=0.08, max=0.5),# m_post_h_p
-              list(min=0.2, max=0.8)#,  #f_h_u_live
-              #list(min=0.25, max=0.9),  # f_h_rr_live # TESTED (fixed)
-              #list(min=0.01, max=1),    # Ch # TESTED (fixed)
-              #list(min=0.1, max=1),    # pii # TESTED (fixed)
-              #list(min=0.2, max=0.4)   #pref_h_u
+              list(min=0, max=0.04),   #m1u
+              #list(min=0.1, max=0.9), #m1p # TESTED (fixed)
+              list(min=0, max=0.02),   #m2u
+              list(min=0.08, max=0.5), #m2p
+              list(min=0.2, max=0.8)#, #fi1_u
+              #list(min=0.25, max=0.9),#RR_fi1 # TESTED (fixed)
+              #list(min=0.01, max=1),  #Uh # TESTED (fixed)
+              #list(min=0.1, max=1),   #pi # TESTED (fixed)
+              #list(min=0.2, max=0.4)  #Pllin
               )
 
 
 # function modelRun encapsulates fRTP_sens function,in a manner to receive a data.frame containing 
 # all parameter combinations and returning the results in one array.
 modelRun <- function (my.data) {
-  return(mapply(fRTP_sens, my.data[,1], my.data[,2], my.data[,3], my.data[,4], my.data[,5], my.data[,6],
+  return(mapply(fRTP_sens, 100, my.data[,1], my.data[,2], my.data[,3], my.data[,4], my.data[,5], my.data[,6],
                 my.data[,7], my.data[,8], my.data[,9], my.data[,10]
                 #, my.data[,11], my.data[,12], my.data[,13], my.data[,14], my.data[,15]
                 ))
@@ -130,19 +132,21 @@ newLHS <- LHS(modelRun, factors, 750, q, q.arg)
 
 
 ### define possible range of parameters from field data
-boxplot(subset(Data_EHT, ITN=="no")$m_pre_h_u.p)
-boxplot(subset(Data_EHT, ITN=="LLIN")$m_pre_h_u.p)
-boxplot(subset(Data_EHT, ITN=="no")$m_post_h_u.p)
-boxplot(subset(Data_EHT, ITN=="LLIN")$m_post_h_u.p)
-boxplot(subset(Data_EHT, ITN=="no")$f_h_u.p_live)
-boxplot(subset(Data_EHT, ITN=="LLIN")$f_h_u.p_live)
 
-hist(subset(Data_EHT, ITN=="no")$m_pre_h_u.p)
-hist(subset(Data_EHT, ITN=="LLIN")$m_pre_h_u.p)
-hist(subset(Data_EHT, ITN=="no")$m_post_h_u.p)
-hist(subset(Data_EHT, ITN=="LLIN")$m_post_h_u.p)
-hist(subset(Data_EHT, ITN=="no")$f_h_u.p_live)
-hist(subset(Data_EHT, ITN=="LLIN")$f_h_u.p_live)
+
+boxplot(subset(Data_EHT, ITN=="no")$m1)
+boxplot(subset(Data_EHT, ITN=="LLIN")$m1)
+boxplot(subset(Data_EHT, ITN=="no")$m2)
+boxplot(subset(Data_EHT, ITN=="LLIN")$m2)
+boxplot(subset(Data_EHT, ITN=="no")$fi1)
+boxplot(subset(Data_EHT, ITN=="LLIN")$fi1)
+
+hist(subset(Data_EHT, ITN=="no")$m1)
+hist(subset(Data_EHT, ITN=="LLIN")$m1)
+hist(subset(Data_EHT, ITN=="no")$m2)
+hist(subset(Data_EHT, ITN=="LLIN")$m2)
+hist(subset(Data_EHT, ITN=="no")$fi1)
+hist(subset(Data_EHT, ITN=="LLIN")$fi1)
 
 
 S = 0.9 # 0.7 to 0.95
