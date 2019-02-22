11bd256c229a638c06097f3f75b95751a4e3ac36
Nicolas MOIROUX <Nicolas.Moiroux@ird.fr>
2019-02-22 10:48:24 +0000

    delete unused parameters in the function

--- R/Fun_fitness_f_kdr.R
+++ R/Fun_fitness_f_kdr.R
@@ -4,8 +4,6 @@
 #' @param Pref_kdr a vector of size three of which elements are the preference for protected LLIN users of the three genotypes (in the following order : RR, RS, SS)
 #' @param m1p_kdr a vector of size three of which elements are the pre-bite mortality of the three genotypes (in the following order : RR, RS, SS) when entering a hut with a LLIN protected individual
 #' @param m2p_kdr a vector of size three of which elements are the pre-bite mortality of the three genotypes (in the following order : RR, RS, SS) when entering a hut with an unprotected individual
-#' @param fi1_u_kdr a vector of size three of which elements are the Successful feeding probability of alive mosquitoes in a hut with an unprotected individual (genotype RR, RS and SS respectively)
-#' @param RR_fi1_kdr a vector of size three of which elements are risk ratio of successful feeding for alive mosquitoes in a hut with LLIN (compared to hut without LLIN)
 #' @param FUN the function used to calculate the number of oviposition events (default = `VLAIB``)
 #'
 #' @return a vector of the relative fitnesses (calculated from the number of oviposition events) of the three genotypes (in the following order: RR RS SS)
@@ -15,11 +13,9 @@
 #' Pllin_kdr <- c(0.6,0.5,0.5)        # from porciani et al 2016
 #' m1p_kdr   <- c(0.05,0.5,0.95)      # from Diop et al 2015
 #' m2p_kdr   <- c(0.005,0.005,0.005)  # default values
-#' fi1_u_kdr <- c(0.55,0.55,0.55)			# default values
-#' RR_fi1_kdr <- c(0.56,0.56,0.56)    # default values
-#' fitness_f_kdr(Pllin_kdr, m1p_kdr, m2p_kdr, success_not, success_net, biting_not, biting_net, VLAIB)
+#' fitness_f_kdr(Pllin_kdr, m1p_kdr, m2p_kdr, VLAIB)
 
-fitness_f_kdr <- function(Pllin_kdr, m1p_kdr, m2p_kdr, fi1_u_kdr, RR_fi1_kdr, FUN=VLAIB){
+fitness_f_kdr <- function(Pllin_kdr, m1p_kdr, m2p_kdr, FUN=VLAIB){
  
   OvA_RR <- FUN( 100, m1p = m1p_kdr[1], m2p = m2p_kdr[1], Pllin = Pllin_kdr[1], RR_fi1 = RR_fi1_kdr[1], fi1_u = fi1_u_kdr[1] )["OvA"]
   OvA_RS <- FUN( 100, m1p = m1p_kdr[2], m2p = m2p_kdr[2], Pllin = Pllin_kdr[2], RR_fi1 = RR_fi1_kdr[2], fi1_u = fi1_u_kdr[2] )["OvA"]
