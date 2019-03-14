3965169c8b5874ae4ac66dd1b3d12db9973da0ed
Nicolas MOIROUX <Nicolas.Moiroux@ird.fr>
2019-03-14 17:25:39 +0000

    add code for supplementary figures

--- R/figures_article.R
+++ R/figures_article.R
@@ -206,7 +206,7 @@ fig4_B <- ggplot(RTP.fit, aes(x=(1-pi.)*100, y=redVC)) +
 	theme(axis.title.x = element_blank())
 
 
-# arrange panels of figure 1
+# arrange panels of figure 4
 figure4 <- ggarrange(fig4_A + rremove("legend"), 
 										 fig4_B + rremove("legend"), 
 										 common.legend = TRUE,
@@ -289,7 +289,7 @@ fig4_B$data %>% filter(pi. == 0.5)
 figure5$data %>% group_by(scn) %>% summarise(val = which.min(abs(fkdr-0.8)))
 
 
-#### Figure X plot preference value recorded by EHT in nature from two meat-analysis studies ----
+#### Figure X plot preference value recorded by EHT in nature from two meta-analysis studies ----
 # calculate prefernce value from field data
 # from EHT in Moiroux et al. 2017, Plos One
 Data_moiroux <- read.delim("data/Data_moiroux.txt")
@@ -317,3 +317,59 @@ Data_pref <- rbind(Data_pref_moiroux, Data_pref_strode)
 
 # plot
 qplot( x=study , y=pLLIN , data=Data_pref , geom=c("boxplot","jitter") ,  ylab="Preference for treated net (pLLIN)")
+
+#### Figure X plot Diversion, pre-bite and post-bite mortality value from Moiroux et al. 2017 ----
+#### justify baseline value (m1, m2, D) from moiroux 2017 :
+Data_moiroux <- read.delim("data/Data_moiroux.txt")	
+
+Data_moiroux %>%
+	dplyr::mutate(m1 = Tot_D_unfd / Total_unfd) %>% 	       # calculate pre-bite mortality
+	dplyr::mutate(m2 = Tot_D_fed / Total_bfed) %>%  	       # calculate post-bite mortality
+	dplyr::mutate(D = Tot_L_unfd / total) -> Data_moiroux2 
+Data_moiroux2 %>%   	           # calculate Diversion rate
+	group_by(ITN) %>% 																			 # group by type of tretament (ITN, CTN or control)
+	summarise_at(c("m1","m2","D"),median, na.rm=TRUE) # calculate mean values of m1, m2 and D
+
+
+Data_moiroux2 %>% 
+	select(ttmt, ITN, m1, m2, D) %>%
+	gather("m1","m2","D", key ="parameter", value = "value") -> plot_parameters
+
+plot_parameters %>%
+	filter(ITN == "no") -> plot_param_UTN
+
+plot_parameters %>%
+	filter(ITN == "ITN") -> plot_param_ITN
+
+fig_param_A <- ggplot(plot_param_UTN, aes(x=parameter, y=value, fill=parameter)) +
+	geom_boxplot(alpha=0.4) +
+	geom_jitter()+
+	stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red") +
+	theme(legend.position="none") +
+	scale_fill_brewer(palette="Set3") +
+	labs(y=" ")+
+	ylim(0,1)+
+	scale_x_discrete(labels=c(expression(paste('D'['U'])), expression(paste('µ'['1U'])), expression(paste('µ'['2U']))))+ 
+	theme(axis.title.x = element_blank())
+
+fig_param_B <- ggplot(plot_param_ITN, aes(x=parameter, y=value, fill=parameter)) +
+	geom_boxplot(alpha=0.4)  +
+	geom_jitter()+
+	stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red") +
+	theme(legend.position="none") +
+	scale_fill_brewer(palette="Set3") +
+	labs(y=" ")+
+	ylim(0,1)+
+	scale_x_discrete(labels=c(expression(paste('D'['P'])), expression(paste('µ'['1P'])), expression(paste('µ'['2P']))))+
+	theme(axis.title.x = element_blank())
+
+figure_param <- ggarrange(fig_param_A, 
+													fig_param_B, 
+													labels = c("A", "B"),
+													ncol = 2, nrow = 1)
+
+annotate_figure(figure_param,
+								bottom = text_grob("Parameters"),
+								left = text_grob("value", rot=90)#,
+								#fig.lab = "Figure 1", fig.lab.face = "bold"
+)
